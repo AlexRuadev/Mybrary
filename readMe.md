8355581039f4bc7e9a43a088276e012015cae954
alexruadev <alexruadev@gmail.com>
2019-11-12 11:05:44 +0700

    Books create index new

--- readMe.md
+++ readMe.md
@@ -2,3 +2,6 @@
 npm install
 <!-- to start the server -->
 npm run dev
+
+<!-- Tuto from  -->
+https://www.youtube.com/watch?v=qj2oDkvc4dQ
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
-const express = require('express');
+const express = require( 'express' );
 const router = express.Router();
+const Book = require( '../models/book' );
 
-router.get('/', (req, res) => {
-    res.render('index')
-})
+router.get( '/', async ( req, res ) => {
+    let books
+    // render book by last added
+    try {
+        books = await Book.find().sort( { createdAt: 'desc' } ).limit( 10 ).exec()
+    } catch {
+        books = []
+    }
+    res.render( 'index', { books: books } )
+} )
 
 module.exports = router
